3aebb046a10145c6df6133e3b8650bd1e52db172
Rick South <rick@natamus.com>
2024-03-09 16:41:55 +0100

    (v5.3) Moved the open door functionality to mixins. [+5]

--- Common/src/main/java/com/natamus/doubledoors/util/Reference.java
+++ Common/src/main/java/com/natamus/doubledoors/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.doubledoors.util;
 public class Reference {
 	public static final String MOD_ID = "doubledoors";
 	public static final String NAME = "Double Doors";
-	public static final String VERSION = "5.2";
+	public static final String VERSION = "5.3";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
