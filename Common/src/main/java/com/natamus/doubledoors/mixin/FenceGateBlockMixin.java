868bf2a96995a8e6dd8721e7fc6f25592563249a
Rick South <rick@natamus.com>
2024-04-22 22:23:46 +0200

    (v5.5) Fixed double fence gates not opening correctly. [+1]

---
+++ Common/src/main/java/com/natamus/doubledoors/mixin/FenceGateBlockMixin.java
@@ -0,0 +1,23 @@
+package com.natamus.doubledoors.mixin;
+
+import com.natamus.doubledoors.events.DoorEvent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.FenceGateBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = FenceGateBlock.class, priority = 1001)
+public class FenceGateBlockMixin {
+	@Inject(method = "use(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/InteractionResult;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/InteractionResult;sidedSuccess(Z)Lnet/minecraft/world/InteractionResult;"))
+	public void use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+		DoorEvent.onDoorClick(level, player, interactionHand, blockPos, blockHitResult);
+	}
+}
--- Common/src/main/java/com/natamus/doubledoors/util/Util.java
+++ Common/src/main/java/com/natamus/doubledoors/util/Util.java
@@ -3,6 +3,7 @@ package com.natamus.doubledoors.util;
 import com.natamus.collective.functions.BlockPosFunctions;
 import com.natamus.doubledoors.config.ConfigHandler;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
@@ -83,7 +84,8 @@ public class Util {
 					continue;
 				}
 				
-				level.setBlock(toOpenBlockPos, oBlockState.setValue(DoorBlock.OPEN, isOpen), 10);
+				Direction facing = blockState.getValue(FenceGateBlock.FACING);
+				level.setBlock(toOpenBlockPos, oBlockState.setValue(DoorBlock.OPEN, isOpen).setValue(FenceGateBlock.FACING, facing), 10);
 			}
 		}
 
