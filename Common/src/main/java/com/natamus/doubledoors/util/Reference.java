1b2a2cdc145dc2dc104f54ab3a64a7c724e4ea32
Rick South <rick@natamus.com>
2024-05-11 09:35:00 +0200

    (v5.7) Added an extra check for incompatibility with other mods that add double doors functionality. [+1]

--- Common/src/main/java/com/natamus/doubledoors/util/Reference.java
+++ Common/src/main/java/com/natamus/doubledoors/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.doubledoors.util;
 public class Reference {
 	public static final String MOD_ID = "doubledoors";
 	public static final String NAME = "Double Doors";
-	public static final String VERSION = "5.5";
+	public static final String VERSION = "5.7";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
