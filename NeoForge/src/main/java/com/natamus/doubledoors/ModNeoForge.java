dac95118e139bff3ddc085bf9ba29efa5b2813e3
Rick South <rick@natamus.com>
2024-07-28 07:36:23 +0200

    (v5.9) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/doubledoors/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/doubledoors/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.doubledoors;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.doubledoors.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.doubledoors.neoforge.events.NeoForgeDoorEvent;
 import com.natamus.doubledoors.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
