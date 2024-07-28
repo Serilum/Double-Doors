329490e4fbb3215ca9b996a4656f402a3c69857b
Rick South <rick@natamus.com>
2024-07-28 07:36:27 +0200

    (v5.9) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/doubledoors/ModFabric.java
+++ Fabric/src/main/java/com/natamus/doubledoors/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.doubledoors;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
 import com.natamus.doubledoors.events.DoorEvent;
 import com.natamus.doubledoors.util.Reference;
@@ -16,6 +17,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
--- NeoForge/src/main/java/com/natamus/doubledoors/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/doubledoors/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.doubledoors;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.doubledoors.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.doubledoors.neoforge.events.NeoForgeDoorEvent;
 import com.natamus.doubledoors.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
