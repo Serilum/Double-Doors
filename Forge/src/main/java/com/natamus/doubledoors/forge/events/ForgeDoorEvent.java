4b38be607e672542bc0dd9fe1bf4aea2eee747e5
Rick South <rick@natamus.com>
2025-02-12 23:37:18 +0100

    (v6.1) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/doubledoors/forge/events/ForgeDoorEvent.java
+++ Forge/src/main/java/com/natamus/doubledoors/forge/events/ForgeDoorEvent.java
@@ -5,12 +5,10 @@ import com.natamus.doubledoors.events.DoorEvent;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeDoorEvent {
 	@SubscribeEvent
-	public void onNeighbourNotice(BlockEvent.NeighborNotifyEvent e) {
+	public static void onNeighbourNotice(BlockEvent.NeighborNotifyEvent e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
