3b6217118fc874c89e7e271392ef2447bb8fe775
Rick South <rick@natamus.com>
2025-03-28 00:53:42 +0100

    Double Doors source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/doubledoors/forge/events/ForgeDoorEvent.java
@@ -0,0 +1,19 @@
+package com.natamus.doubledoors.forge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.doubledoors.events.DoorEvent;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeDoorEvent {
+	@SubscribeEvent
+	public static void onNeighbourNotice(BlockEvent.NeighborNotifyEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		DoorEvent.onNeighbourNotice(level, e.getPos(), e.getState(), e.getNotifiedSides(), e.getForceRedstoneUpdate());
+	}
+}
