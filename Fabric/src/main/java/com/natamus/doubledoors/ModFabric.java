10c20bf17834254273bddf5e8f96d9c4c690d376
Rick South <rick@natamus.com>
2026-03-26 01:55:51 +0100

    Double Doors source code for 26.1.0.

---
+++ Fabric/src/main/java/com/natamus/doubledoors/ModFabric.java
@@ -0,0 +1,42 @@
+package com.natamus.doubledoors;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
+import com.natamus.doubledoors.events.DoorEvent;
+import com.natamus.doubledoors.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.EnumSet;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveBlockEvents.NEIGHBOUR_NOTIFY.register((Level world, BlockPos pos, BlockState state, EnumSet<Direction> notifiedSides, boolean forceRedstoneUpdate) -> {
+			DoorEvent.onNeighbourNotice(world, pos, state, notifiedSides, forceRedstoneUpdate);
+			return true;
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
