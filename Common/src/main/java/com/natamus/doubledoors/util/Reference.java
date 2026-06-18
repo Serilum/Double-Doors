f018254b0c590e8fcdb01626c5bb46ea2ab8489e
Rick South <rick@natamus.com>
2026-06-18 23:59:53 +0200

    Updated Double Doors to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/doubledoors/util/Reference.java
+++ Common/src/main/java/com/natamus/doubledoors/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "doubledoors";
 	public static final String NAME = "Double Doors";
 	public static final String VERSION = "7.2";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
